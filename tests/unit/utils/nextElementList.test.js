78ad6f90c2e55d4ae1b61eb113aad02473e01657
Ruturaj <ruturajhb2003@gmail.com>
2023-07-05 18:41:18 +0530

    Complete Section 12 : Create Headline Component

---
+++ tests/unit/utils/nextElementList.test.js
@@ -0,0 +1,10 @@
+import nextElementInList from '../../../src/utils/nextElementInList';
+
+describe('nextElementInList', () => {
+  it('locates element on list returns the next element in list', () => {
+    const list = ['a', 'b', 'c', 'd'];
+    const value = 'c'
+    const result = nextElementInList(list, value)
+    expect(result).toBe('d')
+  });
+});
