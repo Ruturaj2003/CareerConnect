dfb38549193d8079ebf1f354ec6e120bc26addd8
Ruturaj <ruturajhb2003@gmail.com>
2023-07-17 19:19:35 +0530

    Partailly Complete Section 19 : Dynamix Pagination

--- tests/unit/components/jobResults/JobListings.test.js
+++ tests/unit/components/jobResults/JobListings.test.js
@@ -1,30 +1,105 @@
-import { render, screen } from "@testing-library/vue";
-import { RouterLinkStub } from "@vue/test-utils";
-import axios from "axios";
+import { render, screen } from '@testing-library/vue';
+import { RouterLinkStub } from '@vue/test-utils';
+import axios from 'axios';
 
-import JobListings from "@/components/jobResults/JobListings.vue";
+import JobListings from '@/components/jobResults/JobListings.vue';
 
-vi.mock("axios");
+vi.mock('axios');
 
-describe("JobListings", () => {
-  it("fetches jobs", () => {
-    axios.get.mockResolvedValue({ data: [] });
-    render(JobListings);
-    expect(axios.get).toHaveBeenCalledWith("  http://localhost:3000/jobs");
-  });
+describe('JobListings', () => {
+  const createRoute = (qpam = {}) => {
+    return {
+      query: {
+        page: '1',
+        ...qpam
+      }
+    };
+  };
 
-  it("creates a job listing for every job", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-
+  const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
         stubs: {
-          RouterLink: RouterLinkStub,
+          RouterLink: RouterLinkStub
         },
-      },
+        mocks: {
+          $route
+        }
+      }
+    });
+  };
+
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const $route = createRoute();
+    renderJobListings($route);
+
+    expect(axios.get).toHaveBeenCalledWith('  http://localhost:3000/jobs');
+  });
+
+  it('displays max of 10 jobs', async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const $route = createRoute();
+    renderJobListings($route);
+
+    const jobListings = await screen.findAllByRole('listitem');
+    expect(jobListings).toHaveLength(10);
+  });
+
+  describe('Whem Params exclude page numer', () => {
+    it('displays page number 1', () => {
+      const queryParams = {
+        page: undefined
+      };
+
+      const $route = createRoute(queryParams);
+      renderJobListings($route);
+
+      expect(screen.getByText('Page 1')).toBeInTheDocument;
     });
 
-    const jobListings = await screen.findAllByRole("listitem");
-    expect(jobListings).toHaveLength(15);
+    describe('When Params include page number', () => {
+      it('displays page number', () => {
+        const queryParams = {
+          page: '5'
+        };
+        const $route = createRoute(queryParams);
+        renderJobListings($route);
+        expect(screen.getByText('Page 5')).toBeInTheDocument;
+      });
+    });
+
+    describe('When user is on first page', () => {
+      it('does not show previous page', async () => {
+        axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+        const queryParams = {
+          page: '1'
+        };
+        const $route = createRoute(queryParams);
+        renderJobListings($route);
+        await screen.findAllByRole('listitem');
+
+        const prevLink = screen.queryByRole('link', {
+          name: /previous/i
+        });
+        expect(prevLink).not.toBeInTheDocument();
+      });
+
+      it("shows link to next page", async () => {
+        axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+        const queryParams = {
+          page: '1'
+        };
+        const $route = createRoute(queryParams);
+        renderJobListings($route);
+        await screen.findAllByRole('listitem');
+
+        const nextLink = screen.queryByRole('link', {
+          name: /next/i
+        });
+        expect(nextLink).toBeInTheDocument();
+
+      })
+    });
   });
 });
