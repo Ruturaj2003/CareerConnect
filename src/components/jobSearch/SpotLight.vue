212e9b6d35c05d8b23e0ad84fdcaa1fffb2c8778
Ruturaj <ruturajhb2003@gmail.com>
2023-07-22 17:24:27 +0530

    Complete Section 24: Slots III: Advanced Slots

---
+++ src/components/jobSearch/SpotLight.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="spotLight in spotLights" :key="spotLight.id">
+      <slot :spotlight="spotLight"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'SoptLight',
+  data() {
+    return {
+      spotLights: []
+    };
+  },
+  async mounted() {
+    const baseUrl = import.meta.env.VITE_APP_API_URL;
+    const url = `${baseUrl}/spotLights`;
+    const response = await axios.get(url);
+    this.spotLights = response.data;
+  }
+};
+</script>
