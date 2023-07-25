f6c1e7da55801da5c969d29c121c170e0d5e291e
Ruturaj <ruturajhb2003@gmail.com>
2023-07-25 18:16:59 +0530

    Partially Complete Section 25: Pinia III : Getters

--- src/components/jobResults/jobFiltersSideBar/JobFiltersSideBarOrganizations.vue
+++ src/components/jobResults/jobFiltersSideBar/JobFiltersSideBarOrganizations.vue
@@ -6,7 +6,14 @@
       <fieldset>
         <ul class="flex flex-wrap">
           <li v-for="organiztion in UNIQUE_ORGANIZATIONS" :key="organiztion" class="h-8 w-1/2">
-            <input :id="organiztion" type="checkbox" class="mr-3" />
+            <input
+              :id="organiztion"
+              v-model="selectedOrganization"
+              :value="organiztion"
+              type="checkbox"
+              class="mr-3"
+              @change="selectOrganization"
+            />
             <label :for="organiztion">{{ organiztion }}</label>
           </li>
         </ul>
@@ -21,8 +28,17 @@ import { useJobsStore, UNIQUE_ORGANIZATIONS } from '@/stores/jobs';
 import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue';
 export default {
   name: 'JobFiltersSideBarOrganizations',
+  data() {
+    return {
+      selectedOrganization: []
+    };
+  },
+
   components: {
     CollapsibleAccordion
+  },
+  methods: {
+    selectOrganization() {}
   },
   computed: {
     ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS])
